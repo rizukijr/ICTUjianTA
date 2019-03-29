1a99b84b00aa26b42a1fe8cc3e07d38f564304bc
akbarramadhannnn <akbarramadhan452@gmail.com>
2019-03-29 16:21:40 +0700

    ta-elektro

---
+++ application/modules/koordinatorta/controllers/Koordinatorta.php
@@ -0,0 +1,16 @@
+<?php  
+
+class Koordinatorta extends MX_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+
+	function index()
+	{
+		echo "ini halaman koordinator ta";
+	}
+}
+?>
